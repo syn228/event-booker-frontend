0a092b7d638164296c0ca6c31bbc7f03e18b610b
Aaron Na <syn228@nyu.edu>
2018-09-17 12:59:24 -0700

    small changes for states in calendarpage using datepicker

--- src/components/CalendarPage.js
+++ src/components/CalendarPage.js
@@ -6,21 +6,29 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 class CalendarPage extends Component {
   state = {
-    date: moment()
+    date: moment(),
+    change: false
   }
   
   handleChange = date => {
-    this.setState({ date })
+    this.setState({ date, change: true })
     console.log("hit")
   }
 
   render() {
+    console.log("date", this.state.date._d)
+    console.log("date-type", typeof(this.state.date._d))
+    console.log("change", this.state.change)
     return (
       <div>
+        <p> Please select a date!</p>
         <DatePicker
           onChange={this.handleChange}
           selected={this.state.date}
         />
+        <div>
+        {!this.state.change ? null : <p>You have selected: {JSON.stringify(this.state.date._d)}</p>}
+        </div>
       </div>
     );
   }
