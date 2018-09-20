b2201a4f5cd6801dd582d7e560fe4cf63a5fdbb7
Aaron Na <syn228@nyu.edu>
2018-09-20 11:17:26 -0700

    start adding redux

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { createStore, applyMiddleware } from "redux"
+import reducer from "./src/reducer/reducer"
+import { Provider } from "react-redux"
 
+const store = createStore(reducer)
 
 ReactDOM.render(
+<Provider store={store}>
 <Router>
     <App />
-</Router>, document.getElementById('root'));
+</Router>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
